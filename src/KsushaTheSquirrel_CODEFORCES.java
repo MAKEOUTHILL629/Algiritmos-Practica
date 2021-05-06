f82d1ef9986fdb98b094c2cbf9a144bd113a1fe5
MAKE OUT HILL <felipeh2002@gmail.com>
2021-05-06 17:50:01 -0500

    Se soluciono el ejercio de Ksusha TheSquirrel

--- src/KsushaTheSquirrel_CODEFORCES.java
+++ src/KsushaTheSquirrel_CODEFORCES.java
@@ -1,12 +1,12 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+
+
 //Link https://codeforces.com/problemset/problem/299/B
 public class KsushaTheSquirrel_CODEFORCES {
     public static void main(String[] args) {
-        long tiempoEmpeza = System.currentTimeMillis();
+
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -14,22 +14,51 @@ public class KsushaTheSquirrel_CODEFORCES {
             String[] NandK = br.readLine().split(" ");
             String road = br.readLine();
 
-            Pattern pat =Pattern.compile("#{"+Integer.parseInt(NandK[1])+"}");
-            Matcher mat = pat.matcher(road);
-
-
-            if(!mat.find()){
-                System.out.println("YES");
-            }else{
-                System.out.println("NO");
-            }
-
+            System.out.println(isJump(road, Integer.parseInt(NandK[1])) ? "YES" : "NO");
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        System.out.println(System.currentTimeMillis() - tiempoEmpeza);
+
+    }
+
+    public static boolean isJump(String road, int k) {
+
+        if (k >= road.length()) {
+
+            return true;
+        } else if (!road.contains(Character.toString('#'))) {
+
+            return true;
+        } else {
+
+            for (int i = 0; i < road.length(); i++) {
+                if (i == road.length() - 1) {
+                    return true;
+                } else if (road.charAt(i + 1) == '#') {
+                    boolean bandera = false;
+
+                    for (int j = k; j > 0; j--) {
+                        if ((i + j) >= road.length() - 1) {
+                            return true;
+                        } else if (road.charAt(i + j) == '.') {
+
+                            i += (j - 1);
+                            bandera = true;
+                            break;
+                        }
+                    }
+                    if (!bandera) {
+
+                        return false;
+                    }
+
+                }
+            }
+            return true;
+        }
+
 
     }
 
